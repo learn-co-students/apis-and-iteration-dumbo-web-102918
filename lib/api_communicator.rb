39e382387630b74a4845f944cafcded4b7c8cf97
oazzat <oazzat19@gmail.com>
2018-10-30 16:19:03 -0400

    First Commit

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,14 +2,47 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def do_others (character,det)
+  nex ="http://www.swapi.co/api/people/?page=1"
+
+  while nex != nil
+    response_string = RestClient.get(nex)
+    response_hash = JSON.parse(response_string)
+    # NOTE: in this demonstration we name many of the variables _hash or _array.
+    # This is done for educational purposes. This is not typically done in code.
+    response_hash["results"].each do |char_info|
+      #binding.pry
+      if character == char_info["name"].downcase
+        return char_info[det]
+      end
+    end
+    nex = response_hash["next"]
+  end
+end
+
 def get_character_movies_from_api(character)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
+  nex ="http://www.swapi.co/api/people/?page=1"
 
+  while nex != nil
+  response_string = RestClient.get(nex)
+  response_hash = JSON.parse(response_string)
+  films_array=[]
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
+  # This is done for educational purposes. This is not typically done in code.
+  response_hash["results"].each do |char_info|
+    if character == char_info["name"].downcase
+       char_info["films"].each do |film_link|
+        films_array.push(JSON.parse(RestClient.get(film_link)))
+        end
+        return films_array
+    end
+
+
+  end
+  nex = response_hash["next"]
+
+  end
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -23,14 +56,21 @@ def get_character_movies_from_api(character)
 end
 
 def print_movies(films_hash)
+  films_hash.each do |film|
+    puts film["title"]
+  end
   # some iteration magic and puts out the movies in a nice list
+end
+
+def print_others(item)
+  puts item
 end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
-
+#print_movies(get_character_movies_from_api("Luke Skywalker"))
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
