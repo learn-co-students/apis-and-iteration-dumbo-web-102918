fac7f0a0768e9927f04865bb7528fbd37f535402
mshapir <mshapir95@gmail.com>
2018-10-30 17:49:15 -0400

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,22 @@
+require 'pry'
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome User!"
 end
 
 def get_character_from_user
   puts "please enter a character"
+  character = gets.chomp
+  character.downcase
+  #binding.pry
   # use gets to capture the user's input. This method should return that input, downcased.
 end
+
+def error_response
+  if get_character_from_user == nil
+    puts "Try again!"
+    get_character_from_user
+    binding.pry
+  end
+end
+
+ 0
